8c61dc4679049ef0d1b761b8c9957db519c4c520
unknown <SoloProgrammer>
2023-01-22 14:02:10 +0530

    removed all backend routes and codes

--- index.js
+++ index.js
@@ -2,39 +2,31 @@ const express = require('express')
 
 var cors = require('cors')
 
-require('dotenv').config()
+// require('dotenv').config()
 
-const connetToMongo = require('./config/db.js')
+// const connetToMongo = require('./config/db.js')
 
-const userRoutes = require('./routes/userRoutes')
-const chatRoutes = require('./routes/chatRoutes')
-const messageRoutes = require('./routes/messageRoutes')
+// const userRoutes = require('./routes/userRoutes')
+// const chatRoutes = require('./routes/chatRoutes')
+// const messageRoutes = require('./routes/messageRoutes')
 
-connetToMongo()
+// connetToMongo()
 
-const port = process.env.PORT || 8001
+// const port = process.env.PORT || 8001
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json());
+// app.use(express.json());
 
 // app.get('/', (req, res) => res.send('Server is running........'));
 
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
-app.use('/api/message', messageRoutes);
+// app.use('/api/user', userRoutes);
+// app.use('/api/chat', chatRoutes);
+// app.use('/api/message', messageRoutes);
 
-const server = app.listen(port, () => console.log(` ChatApp Backend is running on server...${port} `))
-
-const io = require('socket.io')(server, {
-    pingTimeout: 120000,
-    cors: {
-        origin: process.env.CLIENT || "http://localhost:3000",
-        methods: ["GET", "POST", "PUT", "DELETE"]
-    }
-});
+// const server = app.listen(port, () => console.log(` ChatApp Backend is running on server...${port} `))
 
 // Cyclick deplyment setup
 const path = require('path')
@@ -45,37 +37,45 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'))
 });
 
-try {
-    io.on("connection", (socket) => {
-        console.log("connected to socket.io");
 
-        socket.on('setup', (userData) => {
-            socket.join(userData._id);
-            socket.emit("connected")
-        })
+// const io = require('socket.io')(server, {
+//     pingTimeout: 120000,
+//     cors: {
+//         origin: process.env.CLIENT || "http://localhost:3000",
+//         methods: ["GET", "POST", "PUT", "DELETE"]
+//     }
+// });
+// try {
+//     io.on("connection", (socket) => {
+//         console.log("connected to socket.io");
 
-        socket.on('join chat', (chatRoom) => {
-            socket.join(chatRoom)
-            console.log("User Joined chatRoom: ", chatRoom)
-        })
+//         socket.on('setup', (userData) => {
+//             socket.join(userData._id);
+//             socket.emit("connected")
+//         })
 
-        socket.on('new message', (newMessageRecieved, Previousmessages) => {
+//         socket.on('join chat', (chatRoom) => {
+//             socket.join(chatRoom)
+//             console.log("User Joined chatRoom: ", chatRoom)
+//         })
 
-            var chat = newMessageRecieved.chat
+//         socket.on('new message', (newMessageRecieved, Previousmessages) => {
 
-            if (!chat) return console.log("chat not defined");
+//             var chat = newMessageRecieved.chat
 
-            if (!chat.users) return console.log("chat.users is not defined");
+//             if (!chat) return console.log("chat not defined");
 
-            chat.users.forEach(user => {
+//             if (!chat.users) return console.log("chat.users is not defined");
 
-                if (user._id == newMessageRecieved.sender._id) return
+//             chat.users.forEach(user => {
 
-                socket.in(user._id).emit("message recieved", newMessageRecieved, Previousmessages)
-            })
+//                 if (user._id == newMessageRecieved.sender._id) return
 
-        })
-    })
-} catch (error) {
-    console.log("Some error occured while connecting with the socket.io ERROR: ", error)
-}   
+//                 socket.in(user._id).emit("message recieved", newMessageRecieved, Previousmessages)
+//             })
+
+//         })
+//     })
+// } catch (error) {
+//     console.log("Some error occured while connecting with the socket.io ERROR: ", error)
+// }   
