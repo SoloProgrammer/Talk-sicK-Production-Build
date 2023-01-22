da399a22ba780dbcda755d88450f5090d04441fa
unknown <SoloProgrammer>
2023-01-22 21:47:44 +0530

    added build configs

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const messageRoutes = require('./routes/messageRoutes')
 
 connetToMongo()
 
-const port = process.env.PORT || 8002
+const port = process.env.PORT || 8001
 
 const app = express()
 app.use(cors())
 
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Server is running........'))
+// app.get('/', (req, res) => res.send('Server is running........'))
 
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
@@ -27,3 +27,11 @@ app.use('/api/message', messageRoutes)
 
 app.listen(port, () => console.log(` ChatApp Backend is running on server...${port} `))
 
+// Cyclick deplyment setup
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, './Frontend/build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'))
+});
